283580054dac33150ba17aab0e200f7c7dae0cc4
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2026-07-24 09:11:44 -0500

    Feature  Plugin Cuando se ejecuta plugin-install Verifica que el ultimo elemento de menu que agrega a aWidget menu = Left.of no debe llevar coma como se indica en el ejemplo
    Widget menu = Left.of(
                    SidebarLogo.of(Icon.LAYER_GROUP, "Ocean"),
                    SidebarCategory.of("Navigation"),
                    ecommMenu,
                    exampleMenu,
                    appsMenu,
                    userManagementMenu,
                    uiKitMenu,
                    uiLayoutMenu,
                    rootUiKitMenu,< el ultimo elemento no debe llevar coma ya que genera un error>
                 
            ).modifier(new io.jettra.flux.core.Modifier().cssClass("professional-left"));
    
    
    
    
    Genere el plugin usando generate-plugin y lo denomine MyExample
    
    Si revisas el codigo del plugin MyExample la pagina PersonaPage.java tiene restriccion @PageWidgetAllow es decir permite el acceso a privilegios ADMIN, Y MANAGER.
    
    
    @JettraPageSincronized(SyncType.ALL)
    @PageWidgetAllow(role = { "ADMIN", "MANAGER" }, department = "")
    @Page(path = "/person")
    public class PersonPage extends TemplatePage { }
    
    Pero cuando ejecuto la aplicacion donde instalo instalo el plugin por ejemplo JettraFluxPluginCemter muestra de manera perfecta el menu, pero al intentar ingresar desde el menu
    a la pagina person o cualquier pagina me redirige al menu, verificar que cuando se loguea las credenciales para @PageWidgetAllow y  @ActionWidgetAllow tomen las del usuario logeado
    en la aplicacion principal y estas se pasen a todos los plugins instalados de manera que 
    las paginas puedan visualizarse siempre y cuando tengan las credenciales.

--- src/main/java/com/flux/example/pages/template/TemplatePage.java
+++ src/main/java/com/flux/example/pages/template/TemplatePage.java
@@ -84,6 +84,66 @@ public abstract class TemplatePage extends FluxBaseHandler {
 
         
         // Menu
+//        WidgetLet ecommMenu = WidgetLet.of("E-Commerce").icon(Icon.HOME);
+//        ecommMenu.add(WidgetLet.of("Dashboard").icon(Icon.CHART_LINE).url(JettraServer.resolvePath("/dashboard")));
+//        ecommMenu.add(WidgetLet.of("Product Overview").icon(Icon.SEARCH).url(JettraServer.resolvePath("/product-overview")));
+//        ecommMenu.add(WidgetLet.of("Product List").icon(Icon.LIST).url(JettraServer.resolvePath("/product-list")));
+//        ecommMenu.add(WidgetLet.of("New Product").icon(Icon.PLUS).url(JettraServer.resolvePath("/new-product")));
+//        ecommMenu.add(WidgetLet.of("Shopping Cart").icon(Icon.SHOPPING_CART).url(JettraServer.resolvePath("/shopping-cart")));
+//        ecommMenu.add(WidgetLet.of("Checkout Form").icon(Icon.CHECK).url(JettraServer.resolvePath("/checkout-form")));
+//        ecommMenu.add(WidgetLet.of("Order History").icon(Icon.HISTORY).url(JettraServer.resolvePath("/order-history")));
+//        ecommMenu.add(WidgetLet.of("Order Summary").icon(Icon.RECEIPT).url(JettraServer.resolvePath("/order-summary")));
+//// Example
+//        WidgetLet exampleMenu = WidgetLet.of("Example").icon(Icon.COG);
+//        exampleMenu.add(WidgetLet.of("Person").icon(Icon.CHART_LINE).url(JettraServer.resolvePath("/person")));
+//        exampleMenu.add(WidgetLet.of("Reglas").icon(Icon.RECEIPT).url(JettraServer.resolvePath("/reglas")));
+//
+//        // Apps
+//        WidgetLet appsMenu = WidgetLet.of("Apps").icon(Icon.TH_LARGE);
+//        appsMenu.add(WidgetLet.of("Chat").icon(Icon.COMMENTS).url(JettraServer.resolvePath("/chat")));
+//        appsMenu.add(WidgetLet.of("Mail Inbox").icon(Icon.ENVELOPE).url(JettraServer.resolvePath("/mail-inbox")));
+//        appsMenu.add(WidgetLet.of("Task List").icon(Icon.CHECK).url(JettraServer.resolvePath("/tasklist")));
+//        appsMenu.add(WidgetLet.of("Files").icon(Icon.FOLDER).url(JettraServer.resolvePath("/files")));
+//        appsMenu.add(WidgetLet.of("File").icon(Icon.FILE).url(JettraServer.resolvePath("/file")));
+//
+//        WidgetLet userManagementMenu = WidgetLet.of("User Management").icon(Icon.USER);
+//        userManagementMenu.add(WidgetLet.of("Profile List").icon(Icon.USERS).url(JettraServer.resolvePath("/profile-list")));
+//        userManagementMenu.add(WidgetLet.of("Basic Information").icon(Icon.INFO_CIRCLE).url(JettraServer.resolvePath("/profile-basic-information")));
+//
+//        WidgetLet uiKitMenu = WidgetLet.of("UI Components").icon(Icon.LAYER_GROUP);
+//        uiKitMenu.add(WidgetLet.of("Input").icon(Icon.EDIT).url(JettraServer.resolvePath("/input")));
+//        uiKitMenu.add(WidgetLet.of("Forms").icon(Icon.ALIGN_JUSTIFY).url(JettraServer.resolvePath("/forms")));
+//        uiKitMenu.add(WidgetLet.of("Icon").icon(Icon.ALIGN_JUSTIFY).url(JettraServer.resolvePath("/icon")));
+//        uiKitMenu.add(WidgetLet.of("Button Demo").icon(Icon.MOUSE_POINTER).url(JettraServer.resolvePath("/button-demo")));
+//        uiKitMenu.add(WidgetLet.of("Table").icon(Icon.CHART_BAR).url(JettraServer.resolvePath("/table")));
+//        uiKitMenu.add(WidgetLet.of("DataView").icon(Icon.LIST).url(JettraServer.resolvePath("/dataview")));
+//        uiKitMenu.add(WidgetLet.of("Tree").icon(Icon.SITEMAP).url(JettraServer.resolvePath("/tree")));
+//        uiKitMenu.add(WidgetLet.of("Panel").icon(Icon.TH_LARGE).url(JettraServer.resolvePath("/panel")));
+//        uiKitMenu.add(WidgetLet.of("Overlay").icon(Icon.CLONE).url(JettraServer.resolvePath("/overlay")));
+//        uiKitMenu.add(WidgetLet.of("Media").icon(Icon.IMAGE).url(JettraServer.resolvePath("/media")));
+//        uiKitMenu.add(WidgetLet.of("Menu").icon(Icon.BARS).url(JettraServer.resolvePath("/menu")));
+//        uiKitMenu.add(WidgetLet.of("Message").icon(Icon.COMMENTS).url(JettraServer.resolvePath("/message")));
+//        uiKitMenu.add(WidgetLet.of("Charts").icon(Icon.CHART_PIE).url(JettraServer.resolvePath("/charts")));
+//        uiKitMenu.add(WidgetLet.of("Timeline").icon(Icon.CALENDAR_ALT).url(JettraServer.resolvePath("/timeline")));
+//        uiKitMenu.add(WidgetLet.of("Misc").icon(Icon.CUBE).url(JettraServer.resolvePath("/misc")));
+//
+//        WidgetLet uiLayoutMenu = WidgetLet.of("Layout & Grid").icon(Icon.WINDOW_MAXIMIZE);
+//        uiLayoutMenu.add(WidgetLet.of("Card Demo").icon(Icon.WINDOW_MAXIMIZE).url(JettraServer.resolvePath("/card-demo")));
+//        uiLayoutMenu.add(WidgetLet.of("Grid Layout").icon(Icon.BORDER_ALL).url(JettraServer.resolvePath("/grid-demo")));
+//
+//        WidgetLet rootUiKitMenu = WidgetLet.of("UI Kit").icon(Icon.LAYER_GROUP);
+//        rootUiKitMenu.add(uiKitMenu);
+//        rootUiKitMenu.add(uiLayoutMenu);
+
+        // --- Left Sidebar (Ocean Style) ---
+
+
+
+
+
+/**
+Start Plugin: MyExample
+**/
         WidgetLet ecommMenu = WidgetLet.of("E-Commerce").icon(Icon.HOME);
         ecommMenu.add(WidgetLet.of("Dashboard").icon(Icon.CHART_LINE).url(JettraServer.resolvePath("/dashboard")));
         ecommMenu.add(WidgetLet.of("Product Overview").icon(Icon.SEARCH).url(JettraServer.resolvePath("/product-overview")));
@@ -136,13 +196,20 @@ public abstract class TemplatePage extends FluxBaseHandler {
         rootUiKitMenu.add(uiLayoutMenu);
 
         // --- Left Sidebar (Ocean Style) ---
+/**
+End Plugin: MyExample
+**/
+
         Widget menu = Left.of(
                 SidebarLogo.of(Icon.LAYER_GROUP, "Ocean"),
                 SidebarCategory.of("Navigation"),
+             
                 ecommMenu,
                 exampleMenu,
                 appsMenu,
                 userManagementMenu,
+                uiKitMenu,
+                uiLayoutMenu,
                 rootUiKitMenu
         ).modifier(new io.jettra.flux.core.Modifier().cssClass("professional-left"));
 
