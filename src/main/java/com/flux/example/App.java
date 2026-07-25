cffd5328ced8f60b97486be198616159d70e4414
avbravo <avbravo@avbravo-IdeaPad-Gaming-3-15ARH7>
2026-07-24 21:37:51 -0500

    Feature  Plugin Añadir a la opción de crear plugin dos nuevo parámetro: exclude-package: Excluye de la generación todos los documentos dentro de los paquetes especificados. exclude-class: Excluye clases especificas que no se desean migrar incluye-test: Indica que se pasarán también los test al plugin. Valores son yes|no
    ./mvn-jettra generate-plugin -path /home/avbravo/Descargas -name MiNuevoPlugin exclude-plugin plugin1,plugin2 exclude-package com.avbravo.general, com.avbravo.prueba exclude-class Clase1.java, Clase2.java incluye-test yes

--- src/main/java/com/flux/example/App.java
+++ src/main/java/com/flux/example/App.java
@@ -33,7 +33,7 @@ public class App {
         IO.println("Iniciando aplicación Web: " + appTitle);
     }
 
-    void main(String[] args) {
+    public static void main(String[] args) {
         if (args != null && args.length > 0 && args[0].equals("-console")) {
             io.jettra.server.autentification.SecurityCLI.main(args);
             return;
