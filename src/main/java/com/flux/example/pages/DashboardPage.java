afe764434d353401831e0a14f2c1826ca732bac5
avbravo <avbravo@avbravo-IdeaPad-Gaming-3-15ARH7>
2026-07-27 21:45:52 -0500

    <id>jitpack.io</id>

--- src/main/java/com/flux/example/pages/DashboardPage.java
+++ src/main/java/com/flux/example/pages/DashboardPage.java
@@ -8,7 +8,7 @@ import io.jettra.flux.core.Widget;
 import io.jettra.core.security.widget.PageWidgetAllow;
 import java.util.Map;
 
-@PageWidgetAllow(role={"ADMIN", "MANAGER"}, department="")
+@PageWidgetAllow(role={jcf.AppRole.ADMIN, jcf.AppRole.MANAGER})
 @io.jettra.core.server.Page(path = "/dashboard")
 public class DashboardPage extends TemplatePage {
 
